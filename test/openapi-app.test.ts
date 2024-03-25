061459d91226cd5a1e1ae134753e9995dd6576f0
Eranda K <emkarachchi@gmail.com>
2024-03-25 15:20:23 +0530

    Initial commit

---
+++ test/openapi-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OpenapiApp from '../lib/openapi-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/openapi-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OpenapiApp.OpenapiAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
